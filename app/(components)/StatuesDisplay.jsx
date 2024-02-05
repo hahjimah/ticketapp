cddcf0374b3edcc1085d79c0ca806299e5724fb8
yk21ju@gmail.com <yk21ju@gmail.com>
2024-02-05 13:39:13 -0800

    CRUD

---
+++ app/(components)/StatuesDisplay.jsx
@@ -0,0 +1,32 @@
+import { get } from "mongoose";
+import React from "react";
+
+const StatuesDisplay = ({ status }) => {
+  const getColor = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "done":
+        color = "bg-green-200";
+        return color;
+      case "started":
+        color = "bg-yellow-200";
+        return color;
+      case "not started":
+        color = "bg-red-300";
+        return color;
+    }
+    return color;
+  };
+
+  return (
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
+      {status}
+    </span>
+  );
+};
+
+export default StatuesDisplay;
